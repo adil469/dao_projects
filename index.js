eee2f27075675e60dcb1e91e9bfe7a6c52762dc8
adil469 <150043307+adil469@users.noreply.github.com>
2023-11-07 13:35:49 +0500

    Add files via upload

--- index.js
+++ index.js
@@ -3,39 +3,10 @@ Object.defineProperty(exports, "__esModule", { value: true });
 var inquirer_1 = require("inquirer");
 var answers = await inquirer_1.default.prompt([
     {
-        type: "number",
-        name: "numberOne",
-        message: "Kindly enter your first no: "
-    },
-    {
-        type: "number",
-        name: "numberTwo",
-        message: "Kindly enter your second no: "
-    },
-    {
-        type: "list",
-        name: "operator",
-        choices: ["*", "+", "-", "/"],
-        message: "Select Operator: "
-    },
+        name: "Sentence",
+        type: "input",
+        message: "Enter your sentence to count the word: "
+    }
 ]);
-var numberOne = answers.numberOne, numberTwo = answers.numberTwo, operator = answers.operator;
-if (numberOne && numberTwo && operator) {
-    var result = 0;
-    if (operator === "+") {
-        result = numberOne + numberTwo;
-    }
-    else if (operator === "-") {
-        result = numberOne - numberTwo;
-    }
-    if (operator === "/") {
-        result = numberOne / numberTwo;
-    }
-    if (operator === "*") {
-        result = numberOne * numberTwo;
-    }
-    console.log("Your result is :", result);
-}
-else {
-    console.log("Kindly enter valid input");
-}
+var words = answers.Sentence.trim().split(" ");
+console.log("Your sentence word count is ".concat(words.length));
